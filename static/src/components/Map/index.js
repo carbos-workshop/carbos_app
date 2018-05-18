d49a78d6cff9824fb7c42169c1829926e1b5e088
Nathan Tutchton <ntutchton@gmail.com>
2018-05-18 15:28:40 -0600

    custom colors and markers

--- static/src/components/Map/index.js
+++ static/src/components/Map/index.js
@@ -1,13 +1,61 @@
 import React, { Component } from 'react'
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import muiThemeable from 'material-ui/styles/muiThemeable';
+import * as actionCreators from '../../actions/theme';
 import {
   Map,
   Polygon,
+  Marker,
   Popup,
   Rectangle,
   TileLayer,
 } from 'react-leaflet'
 
-export default class MapTest extends Component {
+//for custom Marker only
+import L from 'leaflet';
+import styles from './styles.scss'
+
+function mapStateToProps(state) {
+    return {
+        currentTheme: state.theme.currentTheme,
+    };
+}
+
+function mapDispatchToProps(dispatch) {
+    return bindActionCreators(actionCreators, dispatch);
+}
+
+@connect(mapStateToProps, mapDispatchToProps)
+class MapTest extends Component {
+
+  constructor(props) {
+      super(props);
+      this.state = {
+        currentTheme: this.props.currentTheme,
+        url: this.getUrlLink()
+      };
+  }
+
+  //returns true if light, false if dark
+  readCurrentTheme = () => {
+    if (this.props.currentTheme === 'light'){
+      return true
+    }
+    else return false
+  }
+
+  //returns map tile url based on current theme
+  getUrlLink = () => {
+    if (this.readCurrentTheme()){
+        return 'https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png'
+    }
+    if (!this.readCurrentTheme()){
+        return 'https://cartodb-basemaps-{s}.global.ssl.fastly.net/dark_all/{z}/{x}/{y}.png'
+    }
+    else return null
+  }
+
   render() {
     const center = [51.505, -0.09]
 
@@ -20,20 +68,35 @@ export default class MapTest extends Component {
 
     const rectangle = [[51.49, -0.08], [51.5, -0.06]]
 
+    const customMarker = new L.divIcon({className: 'tree'})
+
     return (
       <Map style={{height:'500px'}} center={center} zoom={13}>
         <TileLayer
-          attribution="&amp;copy <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          url={this.getUrlLink()}
+        	attribution= '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="http://cartodb.com/attributions">CartoDB</a>'
+        	subdomains= 'abcd'
         />
-        <Polygon color="purple" positions={polygon} />
-        <Polygon color="purple" positions={multiPolygon}>
+        <Marker
+          position={[51.5,-0.09]}
+          icon={ customMarker }
+        >
+          <Popup>
+            <span>
+              a sample tree marker
+            </span>
+          </Popup>
+        </Marker>
+        <Polygon color={this.props.muiTheme.palette.accent1Color} positions={polygon} />
+        <Polygon color={this.props.muiTheme.palette.accent1Color} positions={multiPolygon}>
         <Popup>
           <span>Popup in Polygon</span>
         </Popup>
         </Polygon>
-        <Rectangle bounds={rectangle} color="black" />
+        <Rectangle bounds={rectangle} color={this.props.muiTheme.palette.primary1Color} />
       </Map>
     )
   }
 }
+
+export default muiThemeable()(MapTest);
