77ede923142e783e22919db12c3dbe0ee33c24b0
Nathan Tutchton <ntutchton@gmail.com>
2018-05-27 16:55:39 -0600

    blurb about how carbos isnt remotely done on home page

--- static/src/components/ProtectedView.js
+++ static/src/components/ProtectedView.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { browserHistory } from 'react-router';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import * as actionCreators from '../actions/data';
@@ -29,18 +30,45 @@ export default class ProtectedView extends React.Component {
         this.props.fetchProtectedData(token);
     }
 
+    dispatchNewRoute(route) {
+        browserHistory.push(route);
+    }
+
     render() {
+
+        const styles = {
+          intro: {
+            color: this.props.muiTheme.palette.textColor,
+          },
+          introText: {
+            marginTop: '4em',
+          },
+          link: {
+            color: this.props.muiTheme.palette.primary1Color,
+            cursor: 'pointer',
+          },
+        }
+
         return (
-            <div >
+            <div style={styles.intro}>
                 {!this.props.loaded
                     ? <h1>Loading data...</h1>
                     :
                     <div>
-                        <h1>Welcome back,
-                            {this.props.userName}!</h1>
-                        <h1>{this.props.data.data.email}</h1>
+                        <h1>Welcome, {this.props.userName}!</h1>
                     </div>
                 }
+                <br />
+                <div style={styles.introText}>
+                  <h3>While Carbos is still under development, we've made some fetures availible for testing.</h3>
+                  <br />
+                  <h3> Visit the
+                    <strong style={styles.link} onClick={()=>{this.dispatchNewRoute('/new/project')}}> New Project MVP </strong>
+                    or
+                    <strong style={styles.link} onClick={()=>{this.dispatchNewRoute('/explore')}}> Explorer </strong>
+                    links to see some of the early features.
+                  </h3>
+                </div>
             </div>
         );
     }
